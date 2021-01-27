344479e774508a63dcd840c3aa4608f804a7fb9e
ravil322 <nasybullin_ravil@mail.ru>
2021-01-27 21:37:52 +0300

    sss

---
+++ RavilBotApp/RavilBotApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RavilBotApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Yo, this is ma boy";
+        }
+
+       
+    }
+}
---
+++ RavilBotApp/RavilBotApp/Models/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RavilBotApp.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://ravilbotapp.azurewebsites.net:443/{0}";
+        public static string Name { get; set; } = "ravil322_bot";
+        public static string Key { get; set; } = "1648296070:AAEtT15Jvv_OcbzlvahUp0hjNrTPGWCxe5g";
+
+
+    }
+}
